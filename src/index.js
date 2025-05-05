45365bfb994f125538ab2ef7b9952be356850247
Olga Baynova (terra456) <terra.456@yandex.ru>
2025-05-05 16:55:10 +0500

    fix: change \n to eol

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ let username;
 argv.forEach((el, i) => {
   if (el.startsWith('--') && el.includes('=')) {
     username = el.split('=')[1];
-    process.stdout.write(`Welcome to the File Manager, ${username}! \n`);
-    process.stdout.write(`You are currently in ${folder}\n`);
+    process.stdout.write(`Welcome to the File Manager, ${username}! ${os.EOL}`);
+    process.stdout.write(`You are currently in ${folder}${os.EOL}`);
   }
 });
 
-const fileSystem = new FileSystem(folder);
+const fileSystem = new FileSystem(folder, os.EOL);
 const operatingSystem = new OperatingSystem(username, folder);
 
 const userInput = new Map([
@@ -41,13 +41,13 @@ process.stdin.on('data', async (data) => {
   const [comand, ...arg] = data.toString().trim().split(' ');
   const operation = userInput.get(comand);
   if (!operation) {
-    process.stdout.write(`Invalid input\n`);
+    process.stdout.write(`Invalid input${os.EOL}`);
   }
   try {
     await operation(...arg);
   } catch (e) {
-    process.stdout.write(`${e.message}\n`);
+    process.stdout.write(`${e.message}${os.EOL}`);
   } finally {
-    process.stdout.write(`You are currently in ${fileSystem.curent}\n`);
+    process.stdout.write(`You are currently in ${fileSystem.curent}${os.EOL}`);
   }
 });
